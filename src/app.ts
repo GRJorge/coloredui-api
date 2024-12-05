3bec9327d04b2b2a633287f56e324f6e1776dcc5
grjorge <deb.devel@outlook.es>
2024-12-05 00:20:14 -0600

    Palette routes

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,15 @@ import cors from "cors";
 import morgan from "morgan";
 import "dotenv/config";
 
+import paletteRouter from "./routes/palette.routes";
+
 const app = express();
 
 app.use(cors());
 app.use(json());
 app.use(urlencoded({ extended: false }));
 app.use(morgan("dev"));
+
+app.use("/palette", paletteRouter);
 
 export default app;
